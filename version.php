5f99e14a3c3cc7f4df5a560f7b5e0447cd39f06b
MaxSannikov2004 <115415063+MaxSannikov2004@users.noreply.github.com>
2025-09-23 12:35:10 +0300

    Update version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin = new stdClass();
 $plugin->component = 'local_admindash';
-$plugin->version   = 2025092306;
-$plugin->release   = '1.1.6';
+$plugin->release   = '1.2.1';
 $plugin->requires  = 2022041900;
 $plugin->maturity  = MATURITY_STABLE;
+$plugin->version   = 2025092322;
